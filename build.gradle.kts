d95d0bca19cd354946589bd8ec37067683c0c649
david dali susanibar arce <davi.sarces@gmail.com>
2022-11-08 19:52:24 -0500

    fix: add build changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -67,19 +67,19 @@ allprojects {
 //                    password = System.getenv("GITHUB_TOKEN").takeUnless { it.isNullOrEmpty() } ?: extra["GITHUB_TOKEN"].toString()
 //                }
 //            }
-            maven {
-                name = "sonatypeLocal"
-                val sonatypeUser = System.getenv("sonatype_user").takeUnless { it.isNullOrEmpty() } ?: extra["sonatype_user"].toString()
-                val sonatypePassword = System.getenv("sonatype_password").takeUnless { it.isNullOrEmpty() } ?: extra["sonatype_password"].toString()
-                val releasesRepoUrl = "http://localhost:8081/repository/maven-releases/"
-                val snapshotsRepoUrl = "http://localhost:8081/repository/maven-snapshots/"
-                isAllowInsecureProtocol = true
-                url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
-                credentials {
-                    username = sonatypeUser
-                    password = sonatypePassword
-                }
-            }
+//            maven {
+//                name = "sonatypeLocal"
+//                val sonatypeUser = System.getenv("sonatype_user").takeUnless { it.isNullOrEmpty() } ?: extra["sonatype_user"].toString()
+//                val sonatypePassword = System.getenv("sonatype_password").takeUnless { it.isNullOrEmpty() } ?: extra["sonatype_password"].toString()
+//                val releasesRepoUrl = "http://localhost:8081/repository/maven-releases/"
+//                val snapshotsRepoUrl = "http://localhost:8081/repository/maven-snapshots/"
+//                isAllowInsecureProtocol = true
+//                url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
+//                credentials {
+//                    username = sonatypeUser
+//                    password = sonatypePassword
+//                }
+//            }
 
 //            maven {
 //                name = "sonatypeCloud"
