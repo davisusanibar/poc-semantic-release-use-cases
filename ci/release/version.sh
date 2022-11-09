63e899c578b869659912eec27dd6eb664aba977c
david dali susanibar arce <davi.sarces@gmail.com>
2022-11-08 20:11:59 -0500

    fix: add build changes

--- ci/release/version.sh
+++ ci/release/version.sh
@@ -3,6 +3,6 @@
 
 set -euo pipefail
 
-LAST=`sed -rn 's/^version = (.*)$/\1/p' ../../gradle.properties`
+LAST=`sed -rn 's/^version = (.*)$/\1/p' gradle.properties`
 echo "Upgrade version from $LAST to $1"
-sed -ir "s/^version = .*/version = $1/" ../../gradle.properties
+sed -ir "s/^version = .*/version = $1/" gradle.properties
