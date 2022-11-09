053ed38798ede868276a84cd02a2dca632da2f5c
david dali susanibar arce <davi.sarces@gmail.com>
2022-11-09 08:33:37 -0500

    fix: test local gradlew

--- ci/release/publish.sh
+++ ci/release/publish.sh
@@ -7,4 +7,5 @@ set -euo pipefail
 #./gradlew publishToMavenLocal -P
 
 #publish to Sonatype maven
+gradle wrapper
 ./gradlew clean publishToSonatype closeSonatypeStagingRepository -PSONATYPE_USER=$SONATYPE_USER -PSONATYPE_PASSWORD=$SONATYPE_PASSWORD -PSIGNING_KEY_ID=$SIGNING_KEY_ID -PSIGNING_PASSWORD=$SIGNING_PASSWORD -PSIGNING_KEY=$SIGNING_KEY
